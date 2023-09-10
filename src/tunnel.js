97e5de25a9bb850a7c2c9b18a8742ec5e0207a12
noreplydev <contact.cristiansanchez@gmail.com>
2023-09-10 19:27:43 +0200

    [FT](Core) Added web server to proxy external requests

--- src/tunnel.js
+++ src/tunnel.js
@@ -1,14 +1,28 @@
 const { createSession, secondsToMs, removeSession } = require('alive-sessions')
 const { nanoid } = require('nanoid')
+const express = require('express')
 const net = require('net')
 
+// http server for consumption
+const app = express()
+
+app.get('*', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(3000, () => {
+  console.log('http server listening on port 3000')
+})
+
+// tunneling service
 const tunnelingServer = net.createServer((socket) => {
   // create a session for each client
   const sessionId = nanoid()
 
+  // create a session and close after timeout
   createSession({
     sessionID: sessionId,
-    expireMs: secondsToMs(20),
+    expireMs: secondsToMs(120),
     action: () => {
       socket.end()
     }
