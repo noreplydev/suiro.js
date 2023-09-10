56394710adf897d76bdfd9c115933b8fa3585cb0
noreplydev <contact.cristiansanchez@gmail.com>
2023-09-10 16:33:26 +0200

    [FT](Seesions)! Now multiples sessions are supported

--- src/tunnel.js
+++ src/tunnel.js
@@ -1,13 +1,30 @@
+const { createSession, secondsToMs, removeSession } = require('alive-sessions')
+const { nanoid } = require('nanoid')
 const net = require('net')
 
 const tunnelingServer = net.createServer((socket) => {
+  // create a session for each client
+  const sessionId = nanoid()
+
+  createSession({
+    sessionID: sessionId,
+    expireMs: secondsToMs(20),
+    action: () => {
+      socket.end()
+    }
+  })
+
   socket.on('connection', (client) => {
     console.log('client connected', client)
   })
 
   socket.on('data', (data) => {
     console.log('Client: ', data.toString())
-    socket.end("This tunnel has been closed")
+  })
+
+  socket.on('end', () => {
+    removeSession(sessionId)
+    console.log('session expired: ', sessionId)
   })
 })
 
